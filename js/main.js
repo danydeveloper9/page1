6ec5bd77a74d7d659a93a2067f4e28ff89cd12bc
dany DEV <59677234+danydeveloper9@users.noreply.github.com>
2020-09-02 02:10:08 -0500

    a2
    actualizacion numero 2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
+
 $(document).ready(function(){
+
+  /* NAVBAR */
+  $("a[href*='#']").click(function(){
+      $("#navbarToggler").collapse("hide");
+  });
 
   /* PAGESCROLL2ID */
   $("a[href*='#']").mPageScroll2id({
-      offset: 20,
+      offset: 5,
       highlightClass: 'active'
+  });
+
+  var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      autoplay: {
+        delay: 5000,
+      },
   });
 
 });
